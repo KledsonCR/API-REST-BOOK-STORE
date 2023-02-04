a0bc80743abf2af54fc7eb9d4eba7b8de3a1f72c
kledson costa <kledsoncrabelo@icloud.com>
2023-02-04 19:01:23 -0300

    feat: add swagger route in index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const Articles = require('./articlesRouter');
+const Swagger = require('./docs/swaggerRouter');
 
 module.exports = (server) => {
-	server.use((req, res, next) => {
-		Articles(server, new Router());
-
-		next();
-	});
+  server.use((req, res, next) => {
+    Articles(server, new Router());
+    Swagger(server, new Router());
+    next();
+  });
 };
