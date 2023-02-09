b05f97f99f147052bc589005a9249d90db2a14ac
kledson costa <kledsoncrabelo@icloud.com>
2023-02-09 17:25:13 -0300

    feat: add route prefix swagger

--- src/routes/swaggerRouter.js
+++ src/routes/swaggerRouter.js
@@ -1,7 +1,7 @@
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('../app/docs/swagger.json');
 
-module.exports = (server, routes, prefix = '/') => {
+module.exports = (server, routes, prefix = '/api/v1') => {
 	routes.use('/', swaggerUi.serve); 
 	routes.get('/', swaggerUi.setup(swaggerDocs));
 	
