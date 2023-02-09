65e29344a0939c3404acad6f26b0927990b1b02e
kledson costa <kledsoncrabelo@icloud.com>
2023-02-09 14:40:20 -0300

    fix: add cors for error correction

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 require('./infra/database/mongo');
 
@@ -10,6 +11,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
   routes() {
